2ca5792ee9c0ee7f59c12169a017d1b27c99458f
Peter Foster <p.foster2@gmail.com>
2020-03-24 14:18:43 +0000

    remove four imports of print_function (Py2->Py3)

--- gram/treegram.py
+++ gram/treegram.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from gram import *
 from p4 import Tree
 
--- gram/treegramradial.py
+++ gram/treegramradial.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from gram.treegram import *
 from p4 import func
 
